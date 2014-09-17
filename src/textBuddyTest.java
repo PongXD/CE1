66e8c1f483572b734367b0562ac6caa7f51aebd2
PongXD <brendanyhy@gmail.com>
2014-09-18 01:02:52 +0800

    Update tests and created resetFile()
    resetFile() just resets the text in the file

--- src/textBuddyTest.java
+++ src/textBuddyTest.java
@@ -5,18 +5,37 @@ import org.junit.Test;
 
 
 public class textBuddyTest {
+	private final String EMPTY_STRING = "";
+	
+	private void resetFile(String fileName){
+		try{
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+			bw.write(EMPTY_STRING);
+			bw.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
 	@Test
 	public void testSort() {
-		textBuddy.testFileContent.add("fox");
-		textBuddy.testFileContent.add("cow");
-		textBuddy.testFileContent.add("200 fishes");
-		textBuddy.testFileContent.add("the hulk");
-		textBuddy.testFileContent.add("foxy mama");
-		textBuddy.copyIntoFileContent();
+		ArrayList<String> testArray = new ArrayList<String>();
+		textBuddy.initialiseTest("myText.txt", testArray);
+		String outputFAILsort = textBuddy.sort();
+		
+		//test output from sort function if no text
+		assertEquals(outputFAILsort, "There is no text in myText.txt to sort!");
+		
+		testArray.add("fox");
+		testArray.add("cow");
+		testArray.add("200 fishes");
+		testArray.add("the hulk");
+		testArray.add("foxy mama");
+		textBuddy.initialiseTest("myText.txt", testArray);
 		String outputSORTED = textBuddy.sort();
 		
-		//output from sort function when completed
+		//test output from sort function when completed
 		assertEquals(outputSORTED, "The text in myText.txt has been sorted alphabetically.");
 		
 		//tests whether the content has been sorted
@@ -27,36 +46,34 @@ public class textBuddyTest {
 		assertEquals("foxy mama", textBuddy.testFileContent.get(3));
 		assertEquals("the hulk", textBuddy.testFileContent.get(4));
 		
-		textBuddy.testFileContent.clear();
-		textBuddy.copyIntoFileContent();
-		String outputFAILsort = textBuddy.sort();
-		
-		//output from sort function if no text
-		assertEquals(output, "There is no text in myText.txt to sort!");
+		resetFile("myText.txt");
 	}
 	
 	@Test
 	public void testSearch() {
-		textBuddy.testFileContent.add("fox");
-		textBuddy.testFileContent.add("cow");
-		textBuddy.testFileContent.add("200 fishes");
-		textBuddy.testFileContent.add("the hulk");
-		textBuddy.testFileContent.add("foxy mama");
-		textBuddy.copyIntoFileContent();
+		ArrayList<String> testArray = new ArrayList<String>();
+		testArray.add("fox");
+		testArray.add("cow");
+		testArray.add("200 fishes");
+		testArray.add("the hulk");
+		testArray.add("foxy mama");
+		textBuddy.initialiseTest("myText.txt", testArray);
 		
-		//output from search function if word can be found
+		//test output from search function if word can be found
 		assertEquals(textBuddy.search("fox"), 
-				"The word \"fox\" has been found these lines\n1. fox\n5. foxy mama");
+				"The word \"fox\" has been found in these lines\n1. fox\n5. foxy mama");
 		assertEquals(textBuddy.search("cow"), 
-				"The word \"cow\" has been found these lines\n2. cow");
+				"The word \"cow\" has been found in these lines\n2. cow");
 		assertEquals(textBuddy.search(" "), 
-				"The word \" \" has been found these lines\n3. 200 fishes\n4. the hulk\n5. foxy mama");
+				"The word \" \" has been found in these lines\n3. 200 fishes\n4. the hulk\n5. foxy mama");
 		
-		//output from the search function if word cannot be found
+		//test output from the search function if word cannot be found
 		assertEquals(textBuddy.search("wolf"), 
 				"The word \"wolf\" cannot be found in myText.txt.");
 		assertEquals(textBuddy.search("foxymama"), 
 				"The word \"foxymama\" cannot be found in myText.txt.");
+		
+		resetFile("myText.txt");
 	}
 
 }
